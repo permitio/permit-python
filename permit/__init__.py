51711c22e0feaff4b4def8a778325a105923798d
Asaf Cohen <asaf@permit.io>
2022-05-23 14:29:45 +0300

    expose more types needed by permit sdk

--- permit/__init__.py
+++ permit/__init__.py
@@ -1,4 +1,26 @@
 from .client import Permit
 from .config import PermitConfig
+from .enforcement.enforcer import Action, Resource, User
+from .enforcement.interfaces import AssignedRole, ResourceInput, UserInput
+from .mutations.client import ReadOperation, WriteOperation
+from .resources.interfaces import ActionConfig, ResourceConfig, ResourceTypes
+from .resources.reporter import ResourceStub
+from .utils.context import Context
 
-__all__ = ["Permit", "PermitConfig"]
+__all__ = [
+    "Permit",
+    "PermitConfig",
+    "User",
+    "Action",
+    "Resource",
+    "Context",
+    "ReadOperation",
+    "WriteOperation",
+    "ActionConfig",
+    "ResourceConfig",
+    "ResourceTypes",
+    "ResourceStub",
+    "UserInput",
+    "ResourceInput",
+    "AssignedRole",
+]
