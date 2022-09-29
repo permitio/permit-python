90e747192620c8654f6aafc017a4f6c9d4ad8706
Asaf Cohen <asaf@permit.io>
2022-09-29 18:04:05 +0300

    fix version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def get_requirements(env=""):
 
 setup(
     name="permit",
-    version="0.0.5-rc",
+    version="1.0.0rc1",
     packages=find_packages(),
     author="Asaf Cohen",
     author_email="asaf@permit.io",
