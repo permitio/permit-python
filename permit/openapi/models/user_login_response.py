be088417bf425689516ea142f57f0f7f95a26727
Oded <oded@permit.io>
2022-12-26 11:07:03 +0200

    update text & version

--- permit/openapi/models/user_login_response.py
+++ permit/openapi/models/user_login_response.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, Field
 
 
 class UserLoginResponse(BaseModel):
-    token: str = Field(..., description="The token to redirect to", title="Token")
+    token: str = Field(..., description="The auth token that lets your users login into permit elements", title="Token")
     redirect_url: str = Field(
         ...,
         description="The full URL to which the user should be redirected in order to complete the login process",
