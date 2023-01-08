9825f3bdd2c69b33ea514938ddf6d059906db098
Oded <oded@permit.io>
2023-01-08 13:58:00 +0200

    Update response obj

--- permit/openapi/models/user_login_response.py
+++ permit/openapi/models/user_login_response.py
@@ -1,10 +1,18 @@
+from typing import Optional
 from pydantic import BaseModel, Field
 
 
 class UserLoginResponse(BaseModel):
-    token: str = Field(..., description="The auth token that lets your users login into permit elements", title="Token")
+    error: Optional[str] = Field(
+        None,
+        description="If the login request failed, this field will contain the error message",
+    )
+    token: Optional[str] = Field(
+        description="The auth token that lets your users login into permit elements"
+    )
+    extra: Optional[str] = Field(
+        None, description="Extra data that you can pass to the login request"
+    )
     redirect_url: str = Field(
-        ...,
-        description="The full URL to which the user should be redirected in order to complete the login process",
-        title="Redirect Url",
+        description="The full URL to which the user should be redirected in order to complete the login process"
     )
