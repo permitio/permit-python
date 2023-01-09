0c34c872cdd187738e9d0b4a44d89950039d4ab7
Oded <oded@permit.io>
2023-01-09 14:11:58 +0200

    update precommit

--- permit/elements.py
+++ permit/elements.py
@@ -1,11 +1,10 @@
 from __future__ import annotations
-from enum import Enum
 
+from enum import Enum
 from typing import TYPE_CHECKING
 from uuid import UUID
 
 from permit.openapi.models.user_login_response import UserLoginAsResponse
-
 
 if TYPE_CHECKING:
     from permit.client import Permit
@@ -17,6 +16,7 @@ class LoginAsErrorMessages(str, Enum):
     INVALID_PERMISSION_LEVEL = "Invalid user permission level"
     FORBIDDEN_ACCESS = "Forbidden access"
 
+
 class PermitElements:
     def __init__(self, client: Permit):
         self._client = client
@@ -25,5 +25,5 @@ class PermitElements:
         self, user_id: str | UUID, tenant_id: str | UUID
     ) -> UserLoginAsResponse:
         ticket = await self._client.api.elements_login_as(user_id, tenant_id)
-        ticket['content'] = {"url": ticket['redirect_url']}
+        ticket["content"] = {"url": ticket["redirect_url"]}
         return ticket
