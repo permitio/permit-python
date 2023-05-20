4422989bf7ce35b16eba97b483c6703b9149c71c
Asaf Cohen <asaf@permit.io>
2023-05-20 22:56:11 +0300

    fix main module exports

--- permit/__init__.py
+++ permit/__init__.py
@@ -1,17 +1,12 @@
+from .api.models import *
 from .config import PermitConfig
 from .enforcement.enforcer import Action, Resource, User
 from .enforcement.interfaces import AssignedRole, ResourceInput, UserInput
+from .exceptions import (
+    PermitApiError,
+    PermitConnectionError,
+    PermitContextError,
+    PermitException,
+)
 from .permit import Permit
 from .utils.context import Context
-
-__all__ = [
-    "Permit",
-    "PermitConfig",
-    "Action",
-    "Resource",
-    "User",
-    "AssignedRole",
-    "ResourceInput",
-    "UserInput",
-    "Context",
-]
