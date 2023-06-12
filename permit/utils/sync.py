8cee9e0afec6665f1e8485257cee38c546f44585
Asaf Cohen <asaf@permit.io>
2023-06-12 15:03:51 +0300

    simplify async_to_sync()

--- permit/utils/sync.py
+++ permit/utils/sync.py
@@ -9,21 +9,10 @@ P = ParamSpec("P")
 T = TypeVar("T")
 
 
-def run_sync(callback: Awaitable[T]) -> T:
-    try:
-        loop = asyncio.get_running_loop()
-    except RuntimeError:
-        # there is no running event loop, so it's safe to use `asyncio.run`
-        return asyncio.run(callback)
-    else:
-        # there *is* a running event loop, so use `loop.run_until_complete`
-        return loop.run_until_complete(callback)
-
-
 def async_to_sync(func: Callable[P, Awaitable[T]]) -> Callable[P, T]:
     @wraps(func)
     def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
-        return run_sync(func(*args, **kwargs))
+        return asyncio.run(func(*args, **kwargs))
 
     return wrapper
 
