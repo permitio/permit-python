7ef4f6ee1eec89da5bd1807894e158a2b1a32841
Asaf Cohen <asaf@permit.io>
2023-06-12 19:00:15 +0300

    fix context api to differentiate between access level and context

--- permit/exceptions.py
+++ permit/exceptions.py
@@ -20,10 +20,21 @@ class PermitConnectionError(PermitException):
 class PermitContextError(Exception):
     """
     The `PermitContextError` class represents an error that occurs when an API method
-    is called with either insufficient level of API key authorization or trying to call
-    an API method that requires a lower level of API key authorization in order to extract
-    implicit context (i.e: most API Methods expects an Environment-level API key so the
-    environment could be implicitly inferred from the API key itself).
+    is called with insufficient context (not knowing in what environment, project or
+    organization the API call is being made).
+
+    Some of the input for the API method is provided via the SDK context.
+    If the context is missing some data required for a method - the api call will fail.
+    """
+
+    pass
+
+
+class PermitContextChangeError(Exception):
+    """
+    The `PermitContextChangeError` will be thrown when the user is trying to set the
+    SDK context to an object that the current API Key cannot access (and if allowed,
+    such api calls will result is 401). Instead the SDK throws this exception.
     """
 
     pass
