9fe2b7397b7f2dd7fdeea4017445b985456a8468
Razco <razchn@gmail.com>
2023-09-20 16:40:37 +0300

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def get_requirements(env=""):
 
 setup(
     name="permit",
-    version="2.1.0rc1",
+    version="2.1.0rc2",
     packages=find_packages(),
     author="Asaf Cohen",
     author_email="asaf@permit.io",
