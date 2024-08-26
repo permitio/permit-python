9a631dce358797d7c248f1d3cc3db3194c382de7
Asaf Cohen <asaf@permit.io>
2024-08-26 15:01:18 +0300

    fix version manually

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def get_readme() -> str:
 
 setup(
     name="permit",
-    version="2.6.1",
+    version="2.6.4",
     packages=find_packages(),
     author="Asaf Cohen",
     author_email="asaf@permit.io",
