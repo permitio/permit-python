930c08f72ac68c8ea54ee848c1e6f9aefa16c984
Gabriel Manor <g.e.manor@gmail.com>
2025-02-10 22:51:51 +0200

    Update permit/enforcement/enforcer.py

--- permit/enforcement/enforcer.py
+++ permit/enforcement/enforcer.py
@@ -483,5 +483,6 @@ class Enforcer:
             raise ValueError(f"permit.check() got invalid resource string: {resource}")
         return ResourceInput(type=parts[0], key=(parts[1] if len(parts) > 1 else None))
 
+
 class SyncEnforcer(Enforcer, metaclass=SyncClass):
     pass
