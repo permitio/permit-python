1a802b0831f3501b0bb7157255d5fa818e7bf888
Gabriel Manor <g.e.manor@gmail.com>
2025-02-10 18:46:39 +0200

    Update test_abac_pdp.py

--- tests/test_abac_pdp.py
+++ tests/test_abac_pdp.py
@@ -1,3 +1,6 @@
+from typing import Any, Dict, List
+
+import aiohttp
 import pytest
 
 from permit import Permit, PermitConnectionError, TenantCreate, UserCreate
@@ -27,9 +30,45 @@ async def test_abac_pdp_cloud_error(permit_cloud: Permit):
                 "attributes": {"private": False},
             },
         )
-
-    except Exception as error:  # noqa: BLE001
+    except (PermitConnectionError, aiohttp.ClientError) as error:
         assert isinstance(error, PermitConnectionError)
+    else:
+        pytest.fail("Should have raised an exception")
 
+
+async def test_get_user_permissions_cloud_error(permit_cloud: Permit):
+    user_test = UserCreate(
+        key="maya@permit.io",
+        email="maya@permit.io",
+        first_name="Maya",
+        last_name="Barak",
+        attributes={"age": 23},
+    )
+
+    try:
+        await permit_cloud.get_user_permissions(
+            user={"key": user_test.key, "email": user_test.email, "attributes": user_test.attributes},
+            tenants=["default"],
+            resources=["Blog:dddddd"],
+            resource_types=["Blog"],
+        )
+    except (PermitConnectionError, aiohttp.ClientError) as error:
+        assert isinstance(error, PermitConnectionError)
+    else:
+        pytest.fail("Should have raised an exception")
+
+
+async def test_filter_objects_cloud_error(permit_cloud: Permit):
+    user_test = {"key": "maya@permit.io", "email": "maya@permit.io", "attributes": {"age": 23}}
+
+    test_resources: List[Dict[str, Any]] = [
+        {"type": "Blog", "key": "doc1", "context": {}, "attributes": {}, "tenant": "default"},
+        {"type": "Document", "key": "doc2", "context": {}, "attributes": {}, "tenant": "default"},
+    ]
+
+    try:
+        await permit_cloud.filter_objects(user=user_test, action="read", context={}, resources=test_resources)
+    except (PermitConnectionError, aiohttp.ClientError) as error:
+        assert isinstance(error, PermitConnectionError)
     else:
         pytest.fail("Should have raised an exception")
