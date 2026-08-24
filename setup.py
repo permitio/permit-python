0d1a03ccd91e8a725bfd95fe3362c7d2dfb4d95d
Tarcio Silva <luan.coc13@gmail.com>
2026-08-24 12:03:38 -0300

    Apply ruff formatting fixes

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=get_requirements(),
     long_description=get_readme(),
     long_description_content_type="text/markdown",
-    package_data={"permit":["py.typed"]},
+    package_data={"permit": ["py.typed"]},
     classifiers=[
         "Operating System :: OS Independent",
         "Programming Language :: Python",
